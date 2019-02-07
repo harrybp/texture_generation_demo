ef36e83760d13aea0d72ae91c1a39b593293f4f2
Harry Bond-Preston <harrytbp@gmail.com>
2019-02-07 13:23:19 +0000

    Changed images

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function gatys_button(){
 function disable_gatys(){
     $('#gatys_button').show()
     $('#gatys_progress_container').hide(); 
+    $('#gatys_progress').css('width', '0%');
     gatys_active = false;
     gatys_progress = 0; 
 }
@@ -30,6 +31,7 @@ function gan_button(){
 function disable_gan(){
     $('#gan_button').show()
     $('#gan_progress_container').hide(); 
+    $('#gan_progress').css('width', '0%');
     gan_active = false;
     gan_progress = 0; 
 }
@@ -40,13 +42,13 @@ function show_source_image(){
     disable_gatys()
     disable_gan()
     var source = $('#source_select').val()
-    $("#source_image").attr("src", 'textures/cropped/'+ source + '.jpg');
+    $("#source_image").attr("src", 'textures/'+ source + '/cropped.jpg');
 }
 
 loaded = 0;
 function increment_loaded(){
     loaded++;
-    if(loaded == 1530){
+    if(loaded == 1380){
         $('#gan_button').removeAttr("disabled");
         $('#gatys_button').removeAttr("disabled");
         $('#both_button').removeAttr("disabled");
@@ -59,13 +61,18 @@ function preload(){
     image_list = ['bricks', 'painting', 'lava', 'pebbles', 'water', 'snake']
     for(var i in image_list){
         console.log(image_list[i])
-        for(var j = 0; j < 255; j+=2){
-            var img1 = new Image();
-            img1.onload = increment_loaded;
-            img1.src = 'textures/gatys/' + image_list[i] + '/' + j + '.jpg'
-            var img2 = new Image()
-            img2.onload = increment_loaded;
-            img2.src = 'textures/gan/' + image_list[i] + '/' + j + '.jpg'
+        for(var j = 0; j < 330; j+=1){
+            if(j < 150){
+                var img1 = new Image();
+                img1.onload = increment_loaded;
+                img1.src = 'textures/' + image_list[i] + '/gatys/' + j + '.jpg'
+            }
+            if(j % 2 == 0){
+                var img2 = new Image()
+                img2.onload = increment_loaded;
+                img2.src = 'textures/' + image_list[i] + '/gan/' + j + '.jpg'
+            }
+            
         }
     }
     
@@ -84,24 +91,46 @@ $(document).ready(function() {
 //-----------------------------------------------------------------------------
 //  Loop
 setInterval(function(){
-    if(gatys_active){
-        gatys_progress+=2;
-        percent = (100/253)*gatys_progress + 1; 
-        $('#gatys_progress').css('width', percent + '%');
-        var source = $('#source_select').val()
-        $("#gatys_image").attr("src", 'textures/gatys/' + source + '/' + gatys_progress + '.jpg');
-        if(gatys_progress > 250){
-            disable_gatys()
-        }  
-    }
+    
     if(gan_active){
         gan_progress+=2;
-        percent = (100/253)*gan_progress + 1; 
+        percent = (100/320)*gan_progress + 1; 
         $('#gan_progress').css('width', percent + '%');
+
+        if(gan_progress < 200)
+            iterations = (gan_progress+1) * 50;
+        else if(gan_progress < 265)
+            iterations = 10000 + (gan_progress - 199)*150
+        else 
+            iterations = 20000 + (gan_progress - 264)*300
+        $('#gan_progress').text(iterations);
+
         var source = $('#source_select').val()
-        $("#gan_image").attr("src", 'textures/gan/' + source + '/' + gan_progress + '.jpg');
-        if(gan_progress > 250){
+        $("#gan_image").attr("src", 'textures/' + source + '/gan/' + gan_progress + '.jpg');
+        if(gan_progress > 330){
             disable_gan()
         } 
     }
 }, 50)
+
+setInterval(function(){
+    if(gatys_active){
+        gatys_progress+=1;
+        percent = (100/64)*gatys_progress + 1; 
+        $('#gatys_progress').css('width', percent + '%');
+        
+        if(gatys_progress < 50)
+            iterations = gatys_progress + 1;
+        else if(gatys_progress < 60)
+            iterations = 50 + (gatys_progress - 49)*5
+        else 
+            iterations = 100 + (gatys_progress - 59)*10
+        $('#gatys_progress').text(iterations);
+
+        var source = $('#source_select').val()
+        $("#gatys_image").attr("src", 'textures/' + source + '/gatys/' + gatys_progress + '.jpg');
+        if(gatys_progress >= 64){
+            disable_gatys()
+        }  
+    }
+}, 130)
